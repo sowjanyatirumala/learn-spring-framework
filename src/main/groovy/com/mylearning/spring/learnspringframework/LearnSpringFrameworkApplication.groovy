60d19a64ce569d4763b27fa43756112d107f8f2f
Sowjanya Tirumala <sowjanyatirumala@outlook.com>
2023-08-06 23:32:15 -0500

    Initial commit

---
+++ src/main/groovy/com/mylearning/spring/learnspringframework/LearnSpringFrameworkApplication.groovy
@@ -0,0 +1,13 @@
+package com.mylearning.spring.learnspringframework
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LearnSpringFrameworkApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkApplication, args)
+	}
+
+}
